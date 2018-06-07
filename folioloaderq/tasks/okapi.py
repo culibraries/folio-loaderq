f7851894d46bc29f756eeda21fafa6e824027ec4
Mark Stacy <mbstacy@gmail.com>
2018-06-07 16:57:01 -0600

    Initial folioloaderq

---
+++ folioloaderq/tasks/okapi.py
@@ -0,0 +1,14 @@
+import requests
+
+host_url =os.getenv('HOSTURL',"http://okapihost")
+
+def okapiHeaders(username,password,tenant='diku'):
+    url = "{0}:9130/authn/login".format(host_url)
+    data={"username": username, "password": password}
+    headers={'Content-Type':'application/json','X-Okapi-Tenant':tenant}
+
+    req = requests.post(url,data=json.dumps(data),headers=headers)
+    if req.status_code >=400:
+        raise Exception("Authentication failed {0}".format(req.text))
+    headers = headers['x-okapi-token']=req.headers['x-okapi-token']
+    return headers
---
+++ folioloaderq/tasks/tasks.py
@@ -0,0 +1,59 @@
+from celery.task import task
+from dockertask import docker_task
+from subprocess import call,STDOUT
+import requests
+from okapi import okapiHeaders
+
+host_url =os.getenv('OKAPIURL',"http://okapihost")
+
+@task()
+def loadFixedDueDateSchedules(data=None,user=None,tenant='diku'):
+    """
+    kwargs:
+        Not required - defaults - override for custom control
+        data= <type list> Fixed Due Date Schedules
+        user= queueconfig.adminuser (file located next to celeryconfig)
+               {"username":"< enter >","password":"< enter >"}
+        tenant= 'diku' - override with string of tenant
+    """
+    if not user:
+        user = getQueueConfig()['okapiAdmin']
+
+    if data:
+        data=json.loads(data)
+    else:
+        #Get sample data no auth needed for get operations
+        data=getCybercomData("catalog","fixedDueDateSchedules")
+
+    headers = okapiHeaders(user['username'],user['password'],tenant)
+    path="fixed-due-date-schedule-storage/fixed-due-date-schedules"
+    added=0
+    errors=0
+    err=[]
+    #post FixedDueDateSchedules to Okapi
+    for fdds in data['results']:
+        try:
+            postOkapiData(fdds,path,headers)
+            addded +=1
+        except Exception as inst:
+            errors +=1
+            err.append(str(inst))
+    return {"Inserted":added,"Errors":{"count":errors,"errors":err}}
+
+def postOkapiData(data,path,headers):
+    url="{0}:9130/{1}".format(host_url,path)
+    req=requests.post(url,json.dumps(data),headers=headers)
+    if req.status_code >=400:
+        raise Exception("Okapi API Error: {0}".format(req.text))
+    return req.json()
+def getCybercomData(database,collection,query='{}',page_size=0):
+    url="{0}:9888/api/data_store/data/{1}/{2}/.json?query={3}&page_size={4}"
+    url= url.format(host_url,database,colllection,query,page_size)
+    req=requests.get(url)
+    if req.status_code >=400:
+        raise Exception("Cybercom API Error: {0}".format(req.text))
+    return req.json()
+
+def getQueueConfig():
+    with open('queueconfig.json','r') as f1:
+        return json.loads(f1.read())
