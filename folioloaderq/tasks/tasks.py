3388f3ca535ca64ba8200cd939ffd2265f367a02
Mark Stacy <mbstacy@gmail.com>
2018-06-07 17:23:54 -0600

    fixed misspelled variable

--- folioloaderq/tasks/tasks.py
+++ folioloaderq/tasks/tasks.py
@@ -47,7 +47,7 @@ def postOkapiData(data,path,headers):
     return req.json()
 def getCybercomData(database,collection,query='{}',page_size=0):
     url="{0}:9888/api/data_store/data/{1}/{2}/.json?query={3}&page_size={4}"
-    url= url.format(host_url,database,colllection,query,page_size)
+    url= url.format(host_url,database,collection,query,page_size)
     req=requests.get(url)
     if req.status_code >=400:
         raise Exception("Cybercom API Error: {0}".format(req.text))
