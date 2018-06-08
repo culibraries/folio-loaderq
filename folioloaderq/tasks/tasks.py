97cfd90de96e4fd04c5bf04c6dcd375ccaee7867
Mark Stacy <mbstacy@gmail.com>
2018-06-08 10:50:13 -0600

    removed and added tag to the query and arguments

--- folioloaderq/tasks/tasks.py
+++ folioloaderq/tasks/tasks.py
@@ -6,7 +6,7 @@ from okapi import okapiHeaders
 host_url =os.getenv('OKAPIURL',"http://okapihost")
 
 @task()
-def loadFixedDueDateSchedules(data=None,user=None,tenant='diku'):
+def loadFixedDueDateSchedules(data=None,user=None,tenant='diku',tag='default'):
     """
     kwargs:
         Not required - defaults - override for custom control
@@ -17,14 +17,12 @@ def loadFixedDueDateSchedules(data=None,user=None,tenant='diku'):
     """
     if not user:
         user = getQueueConfig()['okapiAdmin']
-
     if data:
         data=json.loads(data)
     else:
         #Get sample data no auth needed for get operations
-        query ='{"filter":{},"projection":{"_id":0}}'
+        query ='{{"filter":{{"tag":"{0}"}},"projection":{{"_id":0,"tag":0}}}}'.format(tag)
         data=getSampleData("catalog","fixedDueDateSchedules",query=query)
-    print(user)
     headers = okapiHeaders(user['username'],user['password'],tenant)
     path="fixed-due-date-schedule-storage/fixed-due-date-schedules"
     added=0
@@ -37,7 +35,7 @@ def loadFixedDueDateSchedules(data=None,user=None,tenant='diku'):
             added +=1
         except Exception as inst:
             errors +=1
-            err.append(str(inst))
+            err.append(str(inst).replace('"',''))
     return {"Inserted":added,"Errors":{"count":errors,"errors":err}}
 
 def postOkapiData(data,path,headers):
