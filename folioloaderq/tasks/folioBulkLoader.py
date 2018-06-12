f539cb1479e8681b3b70d1f6d6582e8633bb8149
Mark Stacy <mbstacy@gmail.com>
2018-06-12 17:08:24 -0600

    file adjust

--- folioloaderq/tasks/folioBulkLoader.py
+++ folioloaderq/tasks/folioBulkLoader.py
@@ -1,5 +1,6 @@
 from celery.task import task
 import requests, os,json
+from subprocess import call,check_output,STDOUT
 
 folioDataLoader_url =os.getenv('folioDataLoader_url',"http://dataloader_folio")
 
@@ -7,18 +8,30 @@ folioDataLoader_url =os.getenv('folioDataLoader_url',"http://dataloader_folio")
 def loadMarcRules(json=None):
     headers={'Content-Type':'application/octet-stream','X-Okapi-Tenant':'diku'}
     if json:
-        files = {'file': ('rules.json', json)}
+        filename = 'rules.json'
+        with open('rules.json','w') as f1:
+            f1.write(json)
     else:
-        with open('rules.json','rb') as f1:
-            files = {'file':f1.read()}
+        filename='rules-default.json'
 
+    url = "{0}:8081/load/marc-rules".format(folioDataLoader_url)
+    command = ['curl','-X POST',"-H 'x-okapi-tenant:diku'",
+                "-H 'Content-Type:application/octet-stream'",
+                "-d @{0}".format(filename),url]
+    result=check_output(command)
+    if result =='':
+        return {"status":True,"message":"Rules uploaded","error":''}
+    return {"status":False,"message":"Rules not uploaded","error":result}
+
+
+
+"""
     url = "{0}:8081/load/marc-rules".format(folioDataLoader_url)
     req = requests.post(url, files=files,headers=headers)
     if req.status_code < 400:
         return {"status":True,"message":"Rules uploaded","error":''}
     return {"status":False,"message":"Rules not uploaded","error":req.text}
 
-"""
 curl -X POST -H 'x-okapi-tenant:diku' -H 'Content-Type:application/octet-stream' -d @rules.json    http://localhost:8081/load/marc-rules
 
 """
