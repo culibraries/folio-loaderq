bb234288f46172d08206c216f4b326304ba44f53
Mark Stacy <mbstacy@gmail.com>
2018-06-12 16:31:59 -0600

    added headers to request

--- folioloaderq/tasks/folioBulkLoader.py
+++ folioloaderq/tasks/folioBulkLoader.py
@@ -5,14 +5,14 @@ folioDataLoader_url =os.getenv('folioDataLoader_url',"http://dataloader_folio")
 
 @task()
 def loadMarcRules(json=None):
-    headers = headers={'Content-Type':'application/octet-stream','x-okapi-tenant':'diku'}
+    headers={'Content-Type':'application/octet-stream','X-Okapi-Tenant':'diku'}
     if json:
         files = {'file': ('rules.json', json)}
     else:
         with open('rules.json','rb') as f1:
             files = {'file': ('rules.json', f1.read())}
     url = "{0}:8081/load/marc-rules".format(folioDataLoader_url)
-    req = requests.post(url, files=files)
+    req = requests.post(url, files=files,headers=headers)
     if req.status_code < 400:
         return {"status":True,"message":"Rules uploaded","error":''}
     return {"status":False,"message":"Rules not uploaded","error":req.text}
