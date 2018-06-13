f4780d9d920c1e11215e74debb81d4aba11029a5
Mark Stacy <mbstacy@gmail.com>
2018-06-12 23:36:53 -0600

    updated docstring and signature

--- folioloaderq/tasks/folioBulkLoader.py
+++ folioloaderq/tasks/folioBulkLoader.py
@@ -3,15 +3,22 @@ import requests, os,json
 from subprocess import call,check_output,STDOUT
 
 folioDataLoader_url =os.getenv('folioDataLoader_url',"http://dataloader_folio")
-json_folder= "/vagrant/{0}"
+
 
 @task()
-def loadMarcRules(json=None):
-    headers={'Content-Type':'application/octet-stream','X-Okapi-Tenant':'diku'}
-    if json:
+def loadMarcRules(marc_rules=None):
+    """
+    Load MARC Rules for the bulk data bulk data loader.
+
+    Signature:
+        loadMarcRules(json=None)
+    kwargs:
+        marc_rules - json - default(https://github.com/folio-org/mod-data-loader/blob/master/ramls/rules.json)
+    """
+    if marc_rules:
         filename = 'rules.json'
         with open('rules.json','w') as f1:
-            f1.write(json)
+            f1.write(marc_rules)
     else:
         filename='rulesdefault.json'
 
@@ -28,12 +35,13 @@ def loadMarcRules(json=None):
 
 
 """
+    headers={'Content-Type':'application/octet-stream','X-Okapi-Tenant':'diku'}
+    json_folder= "/vagrant/{0}"
     url = "{0}:8081/load/marc-rules".format(folioDataLoader_url)
     req = requests.post(url, files=files,headers=headers)
     if req.status_code < 400:
         return {"status":True,"message":"Rules uploaded","error":''}
     return {"status":False,"message":"Rules not uploaded","error":req.text}
 
-curl -X POST -H 'x-okapi-tenant:diku' -H 'Content-Type:application/octet-stream' -d @rules.json    http://localhost:8081/load/marc-rules
-
+    curl -X POST -H 'x-okapi-tenant:diku' -H 'Content-Type:application/octet-stream' -d @rules.json    http://localhost:8081/load/marc-rules
 """
